ca690c37fc3e041c5eb5600557a51634f478a7a5
diogo.dmds@gmail.com <DiogoMarcel>
2021-06-03 08:22:52 -0300

    Alter space

--- main.go
+++ main.go
@@ -18,19 +18,16 @@ func main() {
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-	fmt.Println("")
 
-	fmt.Println("Digite uma Expressão Regular para pesquisa (sem espaços):")
+	fmt.Println("\nDigite uma Expressão Regular para pesquisa (sem espaços):")
 	if _, err := fmt.Scanln(&AExpressaoRegular); err != nil {
 		fmt.Println("Error:", err)
 	}
-	fmt.Println("")
 
-	fmt.Println("Digite uma palavra para alterar o texto na busca (sem espaços):")
+	fmt.Println("\nDigite uma palavra para alterar o texto na busca (sem espaços):")
 	if _, err := fmt.Scanln(&AAlterarTexto); err != nil {
 		fmt.Println("Error:", err)
 	}
-	fmt.Println("")
 
 	ARegEx, _ := regexp.Compile(AExpressaoRegular)
 	AMatchString := ARegEx.MatchString(ATextoDigitado)
@@ -39,24 +36,21 @@ func main() {
 	AFindAllString := ARegEx.FindAllString(ATextoDigitado, 100)
 	AReplaceAllString := ARegEx.ReplaceAllString(ATextoDigitado, AAlterarTexto)
 
-	fmt.Println("Resultados:")
+	fmt.Println("\nResultados:")
 	fmt.Println("Texto digitado:", ATextoDigitado)
 	fmt.Println("RegEx digitado:", AExpressaoRegular)
-	fmt.Println("")
-	fmt.Println("Encontrou expressão:", func() string {
+
+	fmt.Println("\nEncontrou expressão:", func() string {
 		if AMatchString {
 			return "SIM"
 		}
 		return "NÃO"
 	}())
-	fmt.Println("")
-	fmt.Println("Texto encontrado:", AFindString)
-	fmt.Println("")
-	fmt.Println("Primeira posição encontrada:", AFindStringIndex)
-	fmt.Println("")
-	fmt.Println("Quantidade encontrada na busca:", len(AFindAllString))
-	fmt.Println("")
-	fmt.Println("Alteração no texto:", AReplaceAllString)
+
+	fmt.Println("\nTexto encontrado:", AFindString)
+	fmt.Println("\nPrimeira posição encontrada:", AFindStringIndex)
+	fmt.Println("\nQuantidade encontrada na busca:", len(AFindAllString))
+	fmt.Println("\nAlteração no texto:", AReplaceAllString)
 
 	fmt.Println("Pressione 'Enter' para continuar...")
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
